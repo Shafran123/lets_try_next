61852131f487ed154bd94c9f131ccabc866675b1
Shafran <Shafran@ieee.org>
2021-03-20 15:36:41 +0530

    pages & routes

---
+++ pages/ninjas/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+              <h1>
+              This is Index Page 
+                </h1> 
+            </div>
+        )
+    }
+}
---
+++ pages/ninjas/test.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Test extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Ninja</h1>
+      </div>
+    )
+  }
+}
