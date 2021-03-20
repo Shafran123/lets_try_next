ea74ef5fc660d1b0394d132f75dbab6bd349f9d7
Shafran <Shafran@ieee.org>
2021-03-20 15:43:24 +0530

    added navbar & footer comp

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Head from 'next/head'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Home Page</h1>
+      <p>Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum </p>
+      <p>Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum </p>
+      <Footer />
     </div>
   )
 }
