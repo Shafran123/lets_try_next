a6350b167ab872a98da4c7b6228ed3392efd8dc1
Shafran <Shafran@ieee.org>
2021-03-21 11:56:50 +0530

    Fetch data getstatic props

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
+import styles from '../../styles/Ninjas.module.css'
+export async function getStaticProps() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+    return {
+        props: {
+            ninjas: data
+        }
+
+    }
+}
 
 export default class index extends Component {
+
+    // static async getStaticProps() {
+    //     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    //     const data = await res.json()
+    //     return { 
+    //             ninjas : data
+    //     }
+    //   }
+
     render() {
+        console.log(this.props.ninjas);
         return (
             <div>
-              <h1>
-              This is Index Page 
-                </h1> 
+                <h1>
+                    All Ninjas
+                </h1>
+                {this.props.ninjas.map(ninja => (
+                    <div key={ninja.id}>
+                        <a className={styles.single}> <h3>{ninja.name}</h3> </a>
+                    </div>
+                ))}
             </div>
         )
     }
